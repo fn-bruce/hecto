37a3061ae4f6f0741baa7c704aa869866954f30d
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-04-22 01:11:52 -0600

    feat: basic foundation
    - Enable raw mode on initialization.
    - Disable raw mode before termination.
    - Clear screen
    - Move cursor to upper left
    - Pressing ESC exits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, stdout};
+
+use crossterm::{
+    cursor::MoveTo,
+    event::{read, Event, KeyCode},
+    execute,
+    terminal::{self, disable_raw_mode, enable_raw_mode},
+};
+
+fn run() -> io::Result<()> {
+    enable_raw_mode()?;
+
+    let mut _stdout = stdout();
+
+    execute!(
+        _stdout,
+        terminal::Clear(terminal::ClearType::All),
+        MoveTo(0, 0)
+    )?;
+
+    loop {
+        let event = read()?;
+
+        if event == Event::Key(KeyCode::Esc.into()) {
+            break;
+        }
+    }
+
+    disable_raw_mode()?;
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    if let Err(e) = run() {
+        println!("Error: {e:?}\r");
+    }
+
+    Ok(())
 }
